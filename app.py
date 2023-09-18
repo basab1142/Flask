ae8478e82c2f96530efd82228dbdce4f6a067195
basab1142 <103180045+basab1142@users.noreply.github.com>
2023-09-18 19:47:50 +0000

    Add dynamic data

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
 from flask import Flask, render_template
+from flask.json import jsonify
 
 app = Flask(__name__)
 
 
+MODELS = [{
+  "id": 1,
+  "title" : "Caption-generator",
+  "built-by" : "basab_g"
+},
+{
+  "id": 2,
+  "title" : "Dog-classification",
+  "built-by" : "nyash"
+},
+{
+  "id": 3,
+  "title" : "Indian-Car-Plate",
+  "built-by" : "nyash"
+}          
+  ]
+
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html", MODELS=MODELS)
+
+@app.route("/api/models")
+def Model_view():
+  return jsonify(MODELS)
+  
 
 
 if __name__ == "__main__":
